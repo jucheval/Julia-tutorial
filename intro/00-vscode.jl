c87dcc588d6477c490637e10a647a377d526ff9e
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2024-12-30 07:41:37 +0100

    init

---
+++ intro/00-vscode.jl
@@ -0,0 +1,11 @@
+# Command Palette
+    # restart REPL (read–eval–print loop)
+    # REPL has four modes :
+    # Julia
+    # ] : packages
+    # ? : help
+    # ; : shell
+    # backspace to return to Julia
+
+# option + clic to get multiple cursors
+# cmd + click to follow links
