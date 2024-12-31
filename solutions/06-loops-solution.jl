9c73e3d64936f6d31bf63fd299948152a98eb2da
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2024-12-31 21:17:16 +0100

    update loops solutions

---
+++ solutions/06-loops-solution.jl
@@ -0,0 +1,48 @@
+# 1. Write a Julia program to construct this pattern below:
+## * 
+## * * 
+## * * * 
+## * * * * 
+## * * * * * 
+## * * * * 
+## * * * 
+## * * 
+## *
+
+rows = [] # empty vector
+for r in 1:5
+    push!(rows, r)
+end
+for r in 4:-1:1
+    push!(rows, r)
+end
+for r in rows
+    println(repeat("* ", r))
+end
+
+# 2. If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+## The sum of these multiples is 23.
+## Find the sum of all the multiples of 3 or 5 that are less or equal than 1000.
+N = 1_000
+acc = 0
+for n in 1:N
+    if n % 3 == 0 || n % 5 == 0
+        acc += n
+    end
+end
+
+# 3. Print all natural numbers from 1 to 10 in reverse
+i = 10
+while i >= 1
+    println(i)
+    i -= 1
+end
+
+# 4. Print all natural *even* numbers from 1 to 10
+i = 1
+while i <= 10
+    if i % 2 == 0
+        println(i)
+    end
+    i += 1
+end
