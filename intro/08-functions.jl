7390fcf02a33d9f8db9ee0396ac06fa11853897c
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2024-12-31 21:17:28 +0100

    v1

--- intro/08-functions.jl
+++ intro/08-functions.jl
@@ -1,19 +1,91 @@
-# functions
+# Functions
+function function_name()
+    return "function output"
+end
+function_name()
 
-# short form
+# Short form
+short_function() = "short function output"
+short_function()
 
-# positional arguments versus keyword arguments
+# Anonymous functions
+(x -> 2*x)(2)
+
+# Positional arguments versus keyword arguments
+function myprint(x, y)
+    println("x = "*string(x)*" and y = "*string(y))
+end
+myprint(1, 2)
+function myprint_with_optional(x, y = 3)
+    println("x = "*string(x)*" and y = "*string(y))
+end
+myprint_with_optional(1)
+myprint_with_optional(1, 2)
+function myprint_with_kwarg(x, y; key1, key2 = 2)
+    println("x = "*string(x)*", y = "*string(y)*", key1 = "*string(key1)*" and key2 = "*string(key2))
+end
+myprint_with_kwarg(1, 2; key1 = 3)
+myprint_with_kwarg(1, 2; key2 = 4, key1 = 2)
+## Arguments before the semicolon (;) are positional (sensible to their position)
+## Arguments after the semicolon (;) are keywords. Not sensible to position but they must be named
+
+function kwarg_only(; key1, key2)
+    println("key1 = "*string(key1)*" and key2 = "*string(key2))
+end
+kwarg_only(; key2 = 4, key1 = 2)
 
 # function vs methods
+## Julia searches the method with the closest supertype of the argument
+myfunction(x) = "General method"    # Corresponds to x::Any
+myfunction(x::Real) = "Real method"
+myfunction(x::Float64) = "Float64 method"
+myfunction(3.1)
+myfunction(Float32(3.1))
+myfunction("3")
 
-# methodswith
+# How to search for methods ?
+methods(myfunction)
+using DrWatson
+methodswith(String, DrWatson; supertypes = true)    # one example where the output list is not too long
 
-# multiple dispatch
+# Multiple dispatch
+myfunction(x::Int, y::Float64) = "Int prevails"
+myfunction(x::Int, y::Int) = "Int prevails and it's a match !"
+myfunction(x::Float64, y::Int) = "Float64 prevails"
+myfunction(x::Float64, y::Float64) = "Float64 prevails and it's a match"
+myfunction(1, 2.)
+myfunction(1, 2)
+myfunction(1., 2)
+myfunction(1., 2.)
 
-# anonymous functions
+# Anonymous functions
+(x -> 2*x)(2)
 
 # map (broadcasting)/reduce/mapreduce
+## map and broadcasting (.) are used to broadcast functions of type T to Vectors{T}
+## In other words, Julia executes a for loop without explicitly writing it
+## note that Any[..] is an artifact of myfunction: 
+## it is needed otherwise the Int's are converted into Float's
+map(myfunction, Any[1, 1, 1., 1.], Any[2., 2, 2, 2.])
+myfunction.(Any[1, 1, 1., 1.], Any[2., 2, 2, 2.])   # Julia performance tips recommend map over the dot syntax
+## filter extracts the values matching a certain condition
+filter(x -> x <= 3, 1:10)
+## reduce applies a binary operator iteratively
+reduce(+, 1:10)     # sum of the first 10 integers
+mapreduce(x -> x^2, +, 1:10)    # sum of the first 10 squares
 
+# ! convention
+## function names ending with ! mutates at least one of their arguments
+x = [6, 5]
+push!(x, 4, 3)
+x
+append!(x, [2, 1])
+x
+sort!(x)
+x
+
+# Respect the types
+## Note that both functions below take arguments of Any type
 function f1(x)
     output = []
     for i in eachindex(x)
@@ -30,14 +102,16 @@ using BenchmarkTools
 @benchmark f1(rand(Int(1e5)))
 @benchmark f2(rand(Int(1e5)))
 f1(["ok since output is Vector{Any}"])
-f1(["error since output is Vector{Float64}"])
+f2(["error since output is Vector{Float64}"])
 
 
-# functions Exercises
+# Exercises
 
-# Make a function `round_number` that rounds a number x as input.
-# It should have two methods:
-# 1. Float64 should use the `round` function
-# 2. Int64 should just return the input (noop)
+# 1. Make a function `round_number` that rounds a number x as input.
+## It should have two methods:
+## a. Float64 should use the `round` function
+## b. Int64 should just return the input
 
-# Write a function to multiply all the numbers in a vector
+# 2. Write a function to multiply all the numbers in a vector
+
+# More info at https://docs.julialang.org/en/v1/manual/functions/
