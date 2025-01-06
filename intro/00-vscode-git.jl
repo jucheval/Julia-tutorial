758fa86cf2a19c54d6dd9957683248b9dd1dafb7
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2025-01-06 10:42:19 +0100

    fusion

--- intro/00-vscode-git.jl
+++ intro/00-vscode-git.jl
@@ -1,3 +1,5 @@
+# VS Code
+
 # Command Palette
     # restart REPL (read–eval–print loop)
     # REPL has four modes :
@@ -9,3 +11,7 @@
 
 # option + clic to get multiple cursors
 # cmd + click to follow links
+
+# Git
+
+# short commits
