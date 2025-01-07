f7a8027d39bcd9e09df916a8b4c9f6059d84a55d
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2025-01-07 12:00:29 +0100

    correction on UnionAll

--- intro/04-types-solution.jl
+++ intro/04-types-solution.jl
@@ -4,8 +4,8 @@ typeof(Int)     # All types have type DataType
 typeof(Union{Int64, Int32})     # finite unions of types have type Union
 
 ## What is the type of Vector ? What is the type of Vector{Int} ? Why ?
-typeof(Vector)   # Vector is the infinite union of Vector{T} for all types T
+typeof(Vector)   # Vector is the union of Vector{T} for all types T : this kind of iterated unions are called UnionAll
 typeof(Vector{Int}) # Vector{Int} is one particular type
 
-# Note that Array{Int} is also UnionAll: it is the infinite union of Array{Int,n} for all integer n
+# Note that Array{Int} is also UnionAll: it is the union of Array{Int,n} for all integer n
 typeof(Array{Int})
