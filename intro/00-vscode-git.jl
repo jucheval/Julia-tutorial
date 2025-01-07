2697c15582818baa7bbf09e42e906e827e6bd8c3
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2025-01-07 12:00:20 +0100

    add git informations

--- intro/00-vscode-git.jl
+++ intro/00-vscode-git.jl
@@ -1,7 +1,8 @@
 # VS Code
 
-# Command Palette
-    # restart REPL (read–eval–print loop)
+# Command Palette (cmd+maj+P or ctrl+maj+P)
+    # search command "Julia: Restart REPL" (read–eval–print loop), the shortcut is displayed and should be alt+j alt+r
+    # It opens a Julia REPL on the bottom panel. This panel can also contain a standard terminal
     # REPL has four modes :
     # Julia
     # ] : packages
@@ -9,9 +10,25 @@
     # ; : shell
     # backspace to return to Julia
 
-# option + clic to get multiple cursors
-# cmd + click to follow links
+# Shortcuts
+## option+click : get multiple cursors
+## cmd+click : follow links
+## alt+cmd+arrows : navigate between tabs
+## alt+arrows : move lines up or down
+## maj+alt+arrows : copy lines up or down
+
+# Code completion (IntelliSense) https://code.visualstudio.com/docs/editor/intellisense
+## It provides lots of (sometimes too many) suggestions. Accept suggestions with <tab>.
+## Snippets are powerful completions. Extensions provide snippets but you can also create your owns. https://code.visualstudio.com/docs/editor/userdefinedsnippets
 
 # Git
+## If you clone a repository from GitHub/GitLab, it becomes a local git repository.
 
-# short commits
+# Standard workflow (in VS Code)
+## Modify your files. Each modified file appears in the Source Control panel.
+## Go to the Source Control panel.
+### Stage the changes you want to commit (+ button on the right of the file name).
+### Type a message in the upper text box which explains your changes. Select the Commit button.
+### Select the Sync Changes button to update your local repository with the remote one.
+
+# Use short commits
