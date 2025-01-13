8f6ed491d6de40eb8dfed0025e888c1f17be7d04
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2025-01-13 11:21:30 +0100

    change unicode and mutation/assignment explanations

--- intro/02-variables.jl
+++ intro/02-variables.jl
@@ -10,17 +10,19 @@ typeof(q)
 
 # Real Numbers: Float64
 y = 3.14
-α = 1.      # character α can be typed by writing "\alpha" and using Tab
 r = 1e3
 typeof(r)
-
-# Numerical constants
-π           # \pi
-ℯ           # \euler
 ## 3 special Float64 values
 Inf
 eps(Float64)
 NaN
+
+# Numerical constants
+## Unicode characters (e.g. greek letters) can be used
+## write \pi and push Tab
+π
+## \euler           
+ℯ
 ## π is not a Float.
 typeof(π)
 ## one reason why
@@ -40,10 +42,13 @@ typeof(letter)
 
 # Assignment vs mutation
 a = [1,2,3]
-b = a         # b points to the same object as a
-a[1] = 42     # mutation : hence b changes too
+b = a
+## b points to the same object as a
+a[1] = 42
+## this is a mutation : hence b changes too
 b
-a = 3.14159   # assignment : b does not change
+a = 3.14159
+## this is an assignment : b does not change
 b
 
 # More info at https://docs.julialang.org/en/v1/manual/variables/
