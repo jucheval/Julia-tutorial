d43e66bf393c3d4aeec3a7ea507547bda91da12a
Julien Chevallier <julien.chevallier1@univ-grenoble-alpes.fr>
2025-01-17 16:37:02 +0100

    formatting

--- perfomance-tips/array-loops-advanced.jl
+++ perfomance-tips/array-loops-advanced.jl
@@ -1,5 +1,6 @@
 using BenchmarkTools
 
+begin
 a = zeros(100,100)
 
 # map and broadcast use the @inbounds macro to avoid bounds checking
@@ -15,7 +16,7 @@ function row_col_loop(a::AbstractArray)
             output[row, col] = a[row, col]^2
         end
     end
-end
+end;
 function col_row_loop(a::AbstractArray)
     output = similar(a)
     nrows, ncols = size(a)
@@ -24,31 +25,31 @@ function col_row_loop(a::AbstractArray)
             output[row, col] = a[row, col]^2
         end
     end
-end
+end;
 function length_loop(a::AbstractArray)
     output = similar(a)
     @inbounds for i in 1:length(a)
         output[i] = a[i]^2
     end
-end
+end;
 function eachindex_loop(a::AbstractArray)
     output = similar(a)
     @inbounds for id in eachindex(a)
         output[id] = a[id]^2
     end
-end
+end;
 function broadcast_loop(a::AbstractArray)
     output = broadcast(x->x^2, a)
-end
+end;
 function dot_broadcast_loop(a::AbstractArray)
     output = a .^ 2
-end
+end;
 function map_loop(a::AbstractArray)
     output = map(x->x^2, a)
-end
+end;
 function comprehension_loop(a::AbstractArray)
     output = [x^2 for x in a]
-end
+end;
 
 ## Mutation functions
 function row_col_loop!(a::AbstractArray)
@@ -58,7 +59,7 @@ function row_col_loop!(a::AbstractArray)
             a[row, col] = a[row, col]^2
         end
     end
-end
+end;
 function col_row_loop!(a::AbstractArray)
     nrows, ncols = size(a)
     @inbounds for col in 1:ncols
@@ -66,64 +67,62 @@ function col_row_loop!(a::AbstractArray)
             a[row, col] = a[row, col]^2
         end
     end
-end
+end;
 function eachindex_loop!(a::AbstractArray)
     @inbounds for id in eachindex(a)
         a[id] = a[id]^2
     end
-end
+end;
 function length_loop!(a::AbstractArray)
     @inbounds for i in 1:length(a)
         a[i] = a[i]^2
     end
-end
+end;
 function broadcast_loop!(a::AbstractArray)
     broadcast!(x->x^2, a, a)
-end
+end;
 function dot_broadcast_loop!(a::AbstractArray)
     a .= a .^ 2
-end
+end;
 function map_loop!(a::AbstractArray)
     map!(x->x^2, a, a)
-end
+end;
 function comprehension_loop!(a::AbstractArray)
     a .= [x^2 for x in a]
-end
+end;
+
+println()
+println("Compute element-wise square of a 100x100 matrix in a new matrix")
+print("row -> col:    "); @btime row_col_loop(a)
+print("col -> row:    "); @btime col_row_loop(a)
+print("eachindex:     "); @btime eachindex_loop(a)
+print("length:        "); @btime length_loop(a)
+print("broadcast:     "); @btime broadcast_loop(a)
+print("dot broadcast: "); @btime dot_broadcast_loop(a)
+print("map:           "); @btime map_loop(a)
+print("comprehension: "); @btime comprehension_loop(a)
+print("row -> col!:   "); @btime row_col_loop!(a)
+print("col -> row!:   "); @btime col_row_loop!(a)
+print("eachindex!:    "); @btime eachindex_loop!(a)
+print("length!:       "); @btime length_loop!(a)
+print("broadcast!:    "); @btime broadcast_loop!(a)
+print("dot broadcast!:"); @btime dot_broadcast_loop!(a)
+print("map!:          "); @btime map_loop!(a)
+print("comprehension!:"); @btime comprehension_loop!(a)
+end;
 
 begin
-    println()
-    println("Compute element-wise square of a 100x100 matrix in a new matrix")
-    print("row -> col:    "); @btime row_col_loop(a);
-    print("col -> row:    "); @btime col_row_loop(a);
-    print("eachindex:     "); @btime eachindex_loop(a);
-    print("length:        "); @btime length_loop(a);
-    print("broadcast:     "); @btime broadcast_loop(a);
-    print("dot broadcast: "); @btime dot_broadcast_loop(a);
-    print("map:           "); @btime map_loop(a);
-    print("comprehension: "); @btime comprehension_loop(a);
-    print("row -> col!:   "); @btime row_col_loop!(a);
-    print("col -> row!:   "); @btime col_row_loop!(a);
-    print("eachindex!:    "); @btime eachindex_loop!(a);
-    print("length!:       "); @btime length_loop!(a);
-    print("broadcast!:    "); @btime broadcast_loop!(a);
-    print("dot broadcast!:"); @btime dot_broadcast_loop!(a);
-    print("map!:          "); @btime map_loop!(a);
-    print("comprehension!:"); @btime comprehension_loop!(a);
-end
-
-
 function sum_loop(a::AbstractArray)
     output = zero(eltype(a))
     @inbounds for x in a
         output += x
     end
     return output
-end
+end;
 
-begin
-    println()
-    println("Compute the sum of a 100x100 matrix")
-    print("sum (alias of reduce(+,x)):");@btime sum(a); # in fact + is replace by add_sum
-    print("reduce:");@btime reduce(+, a);
-    print("loop:");@btime sum_loop(a);
-end
+println()
+println("Compute the sum of a 100x100 matrix")
+print("sum (alias of reduce(+,x)):");@btime sum(a) # in fact + is replace by add_sum
+print("reduce:");@btime reduce(+, a)
+print("loop:");@btime sum_loop(a)
+end;
--- perfomance-tips/array-loops.jl
+++ perfomance-tips/array-loops.jl
@@ -1,6 +1,7 @@
 using BenchmarkTools
 
-a = reshape(100,100)
+begin
+a = zeros(100,100)
 
 function row_col_loop(a::AbstractArray)
     nrows, ncols = size(a)
@@ -10,7 +11,7 @@ function row_col_loop(a::AbstractArray)
             output[row, col] = a[row, col]^2
         end
     end
-end
+end;
 function col_row_loop(a::AbstractArray)
     nrows, ncols = size(a)
     output = zeros(nrows, ncols)
@@ -19,17 +20,19 @@ function col_row_loop(a::AbstractArray)
             output[row, col] = a[row, col]^2
         end
     end
-end
+end;
 function eachindex_loop(a::AbstractArray)
     nrows, ncols = size(a)
     output = zeros(nrows, ncols)
     for id in eachindex(output)
         output[id] = a[id]^2
     end
-end
+end;
+
 println("Compute element-wise square of a 100x100 matrix in a new matrix")
-print("row -> col:"); @btime row_col_loop(a);
-print("col -> row:"); @btime col_row_loop(a);
-print("eachindex: "); @btime eachindex_loop(a);
+print("row -> col:"); @btime row_col_loop(a)
+print("col -> row:"); @btime col_row_loop(a)
+print("eachindex: "); @btime eachindex_loop(a)
 println("The eachindex loop is slower than the col -> row loop because of bounds checking (see the file array-loops-advanced.jl for more details)")
 println("FYI, the balance of speed is inverted when we mutate the input matrix instead of creating a new one")
+end;
